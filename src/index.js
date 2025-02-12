14e8a8763695e01b5cab27f54f0019d474ec57d7
fruitloaf <dmitrisanzharov@gmail.com>
2025-02-12 19:16:05 +0000

    all good

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import { v4 as uuidv4 } from "uuid";
 import generateJoke from './generateJoke';
 import './styles/main.scss';
+import alienSVG from './assets/alien-svgrepo-com.svg';
 
 console.log('123');
 console.log(generateJoke());
 console.log('random id', uuidv4());
 
+const myImg = document.getElementById('myImg');
+myImg.src = alienSVG;
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,55 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        main: path.resolve(__dirname, 'src/index.js')
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
-        clean: true
-    },
-    devServer: {
-        static: path.resolve(__dirname, 'src'),
-        port: 3000,
-        hot: true,
-        open: true,
-        compress: true,
-        historyApiFallback: true
-    },
-    module: {
-        rules: [
+	devtool: "source-map",
+	mode: "development",
+	entry: {
+		main: path.resolve(__dirname, "src/index.js"),
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "[name].[contenthash].js",
+		clean: true,
+        assetModuleFilename: "images/[name][ext]",
+	},
+	devServer: {
+		static: path.resolve(__dirname, "src"),
+		port: 3000,
+		hot: true,
+		open: true,
+		compress: true,
+		historyApiFallback: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: "babel-loader",
+						options: {
+							presets: ["@babel/preset-env"],
+						},
+					},
+				],
+			},
             {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: "asset/resource",
             }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'omg omg 2',
-            filename: 'index.html',
-            template: 'src/template.html' 
-        })
-    ]
-}
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "omg omg 2",
+			filename: "index.html",
+			template: "src/template.html",
+		}),
+	],
+};
