e01c53f070bb14b429fccd480b8796186d22bb2c
fruitloaf <dmitrisanzharov@gmail.com>
2025-02-13 12:47:18 +0000

    all good

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Box, Button } from '@mui/material';
 
 type Props = {}
 
 const App = (props: Props) => {
   return (
-    <div>App</div>
+    <Box>
+        <h1>Hello World</h1>
+        <Button>this is material button</Button>
+    </Box>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(<h1>Hello World</h1>); // here I can also put: root.render(<App />);
+root.render(<App />); // here I can also put: root.render(<App />);
 
