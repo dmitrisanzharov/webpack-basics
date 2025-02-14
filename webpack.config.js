13aa5c27cbd923000c9019a1d368058dac3cf54c
fruitloaf <dmitrisanzharov@gmail.com>
2025-02-14 21:14:31 +0000

    all good

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,18 @@
 const path = require('path');
 
 module.exports = {
-
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        clean: true,
+        assetModuleFilename: 'images/[name][ext]'
+    },
+    module: {
+        rules: [
+        ]
+    }
 };
