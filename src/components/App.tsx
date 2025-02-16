208d2a6a9140d1aee9c3b507c8882a97472a759f
fruitloaf <dmitrisanzharov@gmail.com>
2025-02-16 22:15:13 +0000

    all good

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@mui/material';
+
+type Props = {}
+
+const App = (props: Props) => {
+  return (
+    <Button>App</Button>
+  )
+}
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,64 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+
 module.exports = {
-	mode: "development",
     devtool: "source-map",
-	resolve: {
-		extensions: [".js", ".ts"],
-	},
-	entry: {
-		main: path.resolve(__dirname, "src/index.ts"),
-	},
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "[name].[contenthash].js",
-		clean: true,
-		assetModuleFilename: "images/[name][ext]",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: "asset/resource",
-			},
-			{
-				test: /\.scss$/,
-				use: ["style-loader", "css-loader", "sass-loader"],
-			},
-			{
-				test: /\.(js|ts)$/,
-				exclude: /node_modules/,
-				use: [{ loader: "babel-loader", options: { presets: ["@babel/preset-env", "@babel/preset-typescript"] } }],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: "My Page",
-			filename: "index.html",
-			template: "src/index.html",
-		}),
-	],
+    mode: "development",
+    entry: {
+        main: path.resolve(__dirname, "src/index.tsx"),
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[contenthash].js",
+        clean: true,
+        assetModuleFilename: "images/[name][ext]",
+    },
     devServer: {
         static: path.resolve(__dirname, "src"),
+        port: 3000,
+        hot: true,
         open: true,
         compress: true,
-        historyApiFallback: true
-    }
+        historyApiFallback: true,
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(ts|tsx)$/, // Apply these rules to .ts and .tsx files
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env", "@babel/preset-react"],
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Victors React App",
+            filename: "index.html",
+            template: "src/template.html",
+        }),
+    ],
 };
