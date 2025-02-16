c4325031e9969e2feb4838f0356ee336ba1c0ef1
fruitloaf <dmitrisanzharov@gmail.com>
2025-02-16 19:29:39 +0000

    all good

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import consoleMe from './consoleMe.js';
+import consoleMe from './consoleMe';
 import { v4 as uuidv4 } from "uuid";
+import './styles/styles.scss';
+import mainImg from './assets/chef-man-cap-svgrepo-com.svg'
 
 consoleMe(uuidv4(),'Hello World!');
+
+const image = document.getElementById('myImg');
+image.src = mainImg;
+image.style.height = '100px';
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,42 @@
-const path = require('path');
-console.log('path', path.resolve(__dirname));
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        main: path.resolve(__dirname, 'src/index.js')
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
-        clean: true,
-        assetModuleFilename: 'images/[name][ext]'
-    },
-    module: {
-        rules: [
-            {
-              test: /\.(png|svg|jpg|jpeg|gif)$/i,
-              type: 'asset/resource',
-            },
-
-        ]
-    }
+	mode: "development",
+	resolve: {
+		extensions: [".js"],
+	},
+	entry: {
+		main: path.resolve(__dirname, "src/index.js"),
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "[name].[contenthash].js",
+		clean: true,
+		assetModuleFilename: "images/[name][ext]",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: "asset/resource",
+			},
+			{
+				test: /\.scss$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [{ loader: "babel-loader", options: { presets: ["@babel/preset-env"] } }],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "My Page",
+			filename: "index.html",
+			template: "src/index.html",
+		}),
+	],
 };
