789d8362b8d86659f3c5fb49d80f44df8d0d5617
fruitloaf <dmitrisanzharov@gmail.com>
2025-02-17 17:36:27 +0000

    all good

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,44 @@
-import React from 'react'
-import { Button } from '@mui/material';
 
-type Props = {}
+import React from 'react';
+import { Box } from '@mui/material';
+// @ts-ignore
 
-const App = (props: Props) => {
-  return (
-    <Button>{process.env.SECRET_KEY}</Button>
-  )
+
+type Props = any;
+
+// SX
+const containerSx: any = {
+    display: 'flex',
+    flexDirection: 'row',
+    // justifyContent: 'right',
+    border: '10px solid black',
+    height: '90vh',
+    // margin: '10px',
+    gap: '15px',
+    flexWrap: 'wrap',
 }
 
-export default App
+
+const boxSx: any = {
+    width: '150px',
+    height: '150px',
+    fontSize: '8rem',
+    borderRadius: '15px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+}
+
+
+const App: React.FC<any> = (props: Props) => {
+
+    return <Box sx={containerSx}>
+        <Box sx={{...boxSx, backgroundColor: 'red'}}>1</Box>
+        <Box sx={{...boxSx, backgroundColor: 'yellow'}}>2</Box>
+        <Box sx={{...boxSx, backgroundColor: 'green'}}>3</Box>
+        <Box sx={{...boxSx, backgroundColor: 'blue'}}>4</Box>
+    </Box>;
+};
+
+
+export default App;
