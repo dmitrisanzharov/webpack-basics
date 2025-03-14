1289e0d0012e55e7975cf28d68977ba516f5b45d
fruitloaf <dmitrisanzharov@gmail.com>
2025-03-14 21:46:38 +0000

    all good

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('all running')
+import { v4 as uuidv4 } from 'uuid';
+uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+(
+console.log('all running', uuidv4()));
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+const testConsole = path.resolve(__dirname, 'src/index.js');
+console.log('path', testConsole);
+
+module.exports = {
+
+}
