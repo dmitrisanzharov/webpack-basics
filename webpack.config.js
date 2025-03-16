43e22544d9283974d839fbb37d24599501780795
fruitloaf <dmitrisanzharov@gmail.com>
2025-03-16 17:24:38 +0000

    all good

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,11 @@ const testConsole = path.resolve(__dirname, 'src/index.js');
 console.log('path', testConsole);
 
 module.exports = {
-
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        assetModuleFilename: 'images/[hash][ext]'
+    }
 }
