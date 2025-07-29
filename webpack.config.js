361a7665aa85bd6c38e4776ee3989b238f67d3b5
VictorSarov <victor.sarov@rentalmatics.com>
2025-07-29 15:35:10 +0100

    all done

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,11 @@ console.log("__dirname: ", __dirname);
 
 export default {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        main: './src/index.js',
+    },
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
     }
 };
