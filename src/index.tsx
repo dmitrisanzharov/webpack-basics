540cf43426f524f132e250442fdcfd5bfe8016e9
VictorSarov <victor.sarov@rentalmatics.com>
2025-08-06 17:42:37 +0100

    done

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,4 @@ import ReactDOM from "react-dom/client";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(<h1>Hello World 2</h1>);
+root.render(<h1>Hello World 2 {process.env.VICTOR_SAROV}</h1>);
