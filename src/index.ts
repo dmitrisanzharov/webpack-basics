f8b5c361b09b67d8023ff6be21deadbff873ad82
VictorSarov <victor.sarov@rentalmatics.com>
2025-08-06 13:05:19 +0100

    solved

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import './styles.scss';
 import alien from '@assets/alien'; // image .avif
 import { blah } from '@components/foo'; // .ts
 
-document.getElementById('alienPic').src = alien;
+// document.getElementById('alienPic').src = alien;
 console.log('blah', blah); 
