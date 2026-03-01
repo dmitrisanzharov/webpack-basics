dc2ccc465657bfc22fe572796134ad3efe99403a
fruitloaf <dmitrisanzharov@gmail.com>
2026-03-01 11:26:45 +0000

    f

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import Paper from "@mui/material/Paper";
 
 const root = document.getElementById('root');
 if (!root) {
    throw new Error("Root element not found");
 }
-ReactDOM.createRoot(root).render(<h1>hello world</h1>);
+ReactDOM.createRoot(root).render(<Paper elevation={3} style={{ padding: 16 }}>
+  This is a Paper component
+</Paper>);
