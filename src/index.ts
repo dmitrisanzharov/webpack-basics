7b718bf7e53bdf4dc5eb93cba15969722064cc84
VictorSarov <victor.sarov@rentalmatics.com>
2026-09-08 07:37:46 +0100

    all good

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import '@foo'; 
-import flower from './images/flower.jpg';
+// import flower from './images/flower.jpg';
+import textFile from './file.txt';
+console.log("textFile: ", textFile);
 
 
