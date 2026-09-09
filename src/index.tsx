1afe6ca890ed804b2aef1fa63dde701452a9cf4d
VictorSarov <victor.sarov@rentalmatics.com>
2026-09-09 06:19:00 +0100

    all good

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+// const root = document.getElementById('root');
+
+// if (!root) {
+//    throw new Error("Root element not found");
+// }
+
+// ReactDOM.createRoot(root).render(<h1>Hello</h1>)
